850a5ca69454c66d38d19595c61b582744c7416a
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 22:54:36 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -3,11 +3,11 @@ def find_min_value(array)
   count = 0
    min_value = array[count]
     while count < array.length do
-      
+
       if min_value > array[count]
         min_value = array[count]
       end
       count += 1
     end
-    max_value
+    min_value
 end
